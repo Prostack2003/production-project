7f1df470154ea6767e01aefbc65ed0cda1ed096e
Vladimir <cool.deds@mail.ru>
2024-06-18 20:19:11 +0300

    dev1.0: merge feature/webpack and feature/react

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -11,6 +11,7 @@ export const Counter = () => {
         <div>
             <h1>{counter}</h1>
             <button onClick={increment}>I am Counter!</button>
+            <h2>It's time to Dev!</h2>
         </div>
     );
 };
