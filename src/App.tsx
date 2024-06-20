e9b05488f2487fd63c167a523071d66ec6461182
Vladimir <cool.deds@mail.ru>
2024-06-20 18:16:11 +0300

    feat: add classNames css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/className";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {hovered:true, selected: true}, [theme])}>
             <button onClick={toggleTheme}>Change Theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О Сайте</Link>
