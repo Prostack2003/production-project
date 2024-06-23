833b81f50a97affbac28898856173cd33e99cd4b
Vladimir <cool.deds@mail.ru>
2024-06-23 11:08:15 +0300

    feat: add router provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {classNames} from "../shared/lib/classNames/className";
 import {useTheme} from "./providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "./providers/router";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -14,12 +13,7 @@ const App = () => {
             <button onClick={toggleTheme}>Change Theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О Сайте</Link>
-            <Suspense fallback={<div>Loading</div>}>
-                <Routes>
-                    <Route path="/" element={<MainPage/>}/>
-                    <Route path="/about" element={<AboutPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
